b38d91f4d437297bbcd4f6aa8df45665f3926a1f
Ziyaad <xavierbaatjes@yahoo.com>
2023-10-04 14:21:05 +0200

    Update data.py

--- data.py
+++ data.py
@@ -17,14 +17,10 @@ from text.symbols import symbols
 from utils import parse_filelist, intersperse
 from model.utils import fix_len_compatibility
 from params import seed as random_seed
-
-import sys
-sys.path.insert(0, 'hifi-gan')
 from librosa.filters import mel as librosa_mel_fn
-import os
+
+
 # create spectrogram
-mel_basis = {}
-hann_window = {}
 
 def dynamic_range_compression_torch(x, C=1, clip_val=1e-5):
     return torch.log(torch.clamp(x, min=clip_val) * C)
@@ -33,30 +29,34 @@ def spectral_normalize_torch(magnitudes):
     output = dynamic_range_compression_torch(magnitudes)
     return output
 
-def mel_spectrogram(y, n_fft, num_mels, sampling_rate, hop_size, win_size, fmin, fmax, center=False):
-    global mel_basis
-    if str(fmax) not in mel_basis:
-        mel_basis[str(fmax)] = librosa_mel_fn(sr=sampling_rate, n_fft=n_fft, n_mels=num_mels, fmin=fmin, fmax=fmax)
+mel_basis = {}
+hann_window = {}
 
-    # Padding
-    y = torch.nn.functional.pad(y.unsqueeze(1), (int((n_fft - hop_size) / 2), int((n_fft - hop_size) / 2)), mode='reflect')
+
+def mel_spectrogram(y, n_fft, num_mels, sampling_rate, hop_size, win_size, fmin, fmax, center=False):
+    if torch.min(y) < -1.:
+        print('min value is ', torch.min(y))
+    if torch.max(y) > 1.:
+        print('max value is ', torch.max(y))
+
+    global mel_basis, hann_window
+    if fmax not in mel_basis:
+        mel = librosa_mel_fn(sampling_rate, n_fft, num_mels, fmin, fmax)
+        mel_basis[str(fmax)+'_'+str(y.device)] = torch.from_numpy(mel).float().to(y.device)
+        hann_window[str(y.device)] = torch.hann_window(win_size).to(y.device)
+
+    y = torch.nn.functional.pad(y.unsqueeze(1), (int((n_fft-hop_size)/2), int((n_fft-hop_size)/2)), mode='reflect')
     y = y.squeeze(1)
 
-    # Short-time Fourier transform
-    hann_window = torch.hann_window(win_size)
-    spec = torch.stft(y, n_fft, hop_length=hop_size, win_length=win_size, window=hann_window,
-                      center=center, pad_mode='reflect', normalized=False, onesided=True, return_complex=True)
+    spec = torch.stft(y, n_fft, hop_length=hop_size, win_length=win_size, window=hann_window[str(y.device)],
+                      center=center, pad_mode='reflect', normalized=False, onesided=True)
 
-    # Magnitude spectrogram
-    spec_mag = torch.abs(spec)
+    spec = torch.sqrt(spec.pow(2).sum(-1)+(1e-9))
 
-    # Mel spectrogram
-    mel = torch.matmul(torch.tensor(mel_basis[str(fmax)]).to(y.device), spec_mag)
+    spec = torch.matmul(mel_basis[str(fmax)+'_'+str(y.device)], spec)
+    spec = spectral_normalize_torch(spec)
 
-    # Spectral normalization
-    mel = spectral_normalize_torch(mel)
-
-    return mel
+    return spec
 
 
 class TextMelDataset(torch.utils.data.Dataset):
