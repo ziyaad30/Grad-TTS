eb11189f053da99a0692714b00f80374bef0b025
Ziyaad <xavierbaatjes@yahoo.com>
2023-10-04 16:20:52 +0200

    Update data.py

--- data.py
+++ data.py
@@ -17,9 +17,6 @@ from text.symbols import symbols
 from utils import parse_filelist, intersperse
 from model.utils import fix_len_compatibility
 from params import seed as random_seed
-
-import sys
-sys.path.insert(0, 'hifi-gan')
 from meldataset import mel_spectrogram
 
 
