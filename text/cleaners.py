abf1922a310a88ef48d6adcf76ca955d71551302
Ziyaad <xavierbaatjes@yahoo.com>
2023-11-19 04:25:39 +0200

    Add files via upload

--- text/cleaners.py
+++ text/cleaners.py
@@ -1,9 +1,6 @@
 import re
-import os
 from unidecode import unidecode
 from .numbers import normalize_numbers
-
-
 
 # Regular expression matching whitespace:
 _whitespace_re = re.compile(r"\s+")
@@ -36,6 +33,7 @@ _abbreviations = [
 
 
 def expand_abbreviations(text):
+    """Check and replace common abbreviations with their full words."""
     for regex, replacement in _abbreviations:
         text = re.sub(regex, replacement, text)
     return text
@@ -58,42 +56,53 @@ def convert_to_ascii(text):
 
 
 def check_stops(text):
-	for test in text.split(" "):
-		ellipses = re.search(r'(\w+)\.{1,}', test)
-		if ellipses:
-			word = ellipses.group(0)
-			elip = word[0:-1]
-			text = text.replace(word, elip + " .")
-	return text
+    for test in text.split(" "):
+        ellipses = re.search(r'(\w+)\.{1,}', test)
+        if ellipses:
+            word = ellipses.group(0)
+            elip = word[0:-1]
+            text = text.replace(word, elip + " .")
+    return text
 
 
 def check_ellipse(text):
-	for test in text.split(" "):
-		ellipses = re.search(r'(\w+)\.{3,}', test)
-		if ellipses:
-			word = ellipses.group(0)
-			elip = word[0:-3]
-			text = text.replace(word, elip + " ...")
-	return text
+    for test in text.split(" "):
+        ellipses = re.search(r'(\w+)\.{3,}', test)
+        if ellipses:
+            word = ellipses.group(0)
+            elip = word[0:-3]
+            text = text.replace(word, elip + " ...")
+    return text
+
+
+def check_arpa_stress(text):
+    """Pipeline for arpabet text, including number and abbreviation expansion but checks for arpabet stress."""
+    text = text.split()
+    new_text = []
+    for t in text:
+        if t.endswith(')') or t.endswith('),') or t.endswith(')?') or t.endswith(')!') or t.endswith('):'):
+            new_text.append(t)
+        else:
+            new_text.append(expand_numbers(t))
+
+    return ' '.join(new_text)
 
 
 def english_cleaners(text):
-	'''Pipeline for English text, including number and abbreviation expansion.'''
-	text = text.strip()
-	text = convert_to_ascii(text)
-	text = lowercase(text)
-	text = expand_numbers(text)
-	text = expand_abbreviations(text)
-	text = collapse_whitespace(text)
-	text = text.replace('(', '')
-	text = text.replace(')', '')
-	text = text.replace(':', ' : ')
-	text = text.replace(';', ' ;')
-	text = text.replace('"', '')
-	text = text.replace('!', ' !')
-	text = text.replace('?', ' ?')
-	text = text.replace(',', ' ,')
-	text = text.replace('-', ' ')
-	text = check_ellipse(text)
-	text = check_stops(text)
-	return text
+    """Pipeline for English text, including number and abbreviation expansion."""
+    text = text.strip()
+    text = convert_to_ascii(text)
+    text = lowercase(text)
+    text = check_arpa_stress(text)
+    text = expand_abbreviations(text)
+    text = collapse_whitespace(text)
+    text = text.replace('--', '-')
+    text = text.replace(':', ' : ')
+    text = text.replace(';', ' ;')
+    text = text.replace('"', '')
+    text = text.replace('!', ' !')
+    text = text.replace('?', ' ?')
+    text = text.replace(',', ' ,')
+    text = check_ellipse(text)
+    text = check_stops(text)
+    return text
